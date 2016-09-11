010a9a144dad6f21ff6d7f4452ad3aa1a47d6826
zhangjunmin <zhangjunmin@wedocare.com>
2016-09-11 22:14:02 +0800

    添加视图

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express'),
 	PORT = process.env.PORT || 3000;
-	app = express();
+	app = express(),
+	path = require('path');
 
-
+app.set('view engine','jade');
+app.set('views','views')
+app.use(express.static(path.join(__dirname,'libs')));
 app.get('/',function (req,res) {
 	// body...
-	res.send('index');
+	res.render('index');
 })
 
 app.get('/goods',function (req,res) {
